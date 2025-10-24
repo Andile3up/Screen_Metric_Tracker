fe1bfab7bfbc498a5073a0fe588d0060f19e0166
Andile3up <andile@3upads.com>
2025-10-25 00:29:01 +0200

    add startup script

---
+++ startup.sh
@@ -0,0 +1,6 @@
+! /usr/bin/bash
+#git pull /// figure out a way of doing git pull more effciently
+#gcc main.c -o runn
+sleep 30
+nohup /home/admin3upads/Screen_Metric_Tracker/serial_logger
+#touch /home/admin3upads/Screen_Metric_Tracker/comming_from
