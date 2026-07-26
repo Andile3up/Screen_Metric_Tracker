37874c2c5dd6a1fb9e3655d28bd43a83dc52baf5
Andile3up <andile@3upads.com>
2026-07-26 20:22:41 +0200

    Update collector2.py

--- collector2.py
+++ collector2.py
@@ -1,5 +1,19 @@
+import sys
 import pandas as pd
-df = pd.read_csv('tracker/output.txt', header=None,names=["mac_addr","start_time","end_time"])
+
+print("Total arguments:", len(sys.argv))
+print("Script name:", sys.argv[0])
+print("Arguments:", sys.argv[1])
+file_name=sys.argv[1]
+
+file_split=file_name.split("/")
+process_file_name=file_split[-1]
+process_file_name=process_file_name.split(".")
+process_file_name=process_file_name[0]
+print(process_file_name)
+
+df = pd.read_csv(file_name, header=None,names=["mac_addr","start_time","end_time"])
 print(df)
 print(df.dtypes)
-df.to_parquet('output.parquet', compression='gzip')
+df.to_parquet("data/Impressions/upload/"+process_file_name+".parquet", compression='gzip')
+#sys.exit(1)
